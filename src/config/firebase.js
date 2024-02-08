edd5bdf4b7868c9469ec1cb7307329780799483b
yr33 <yuvaraj.r@dxc.com>
2024-02-08 20:52:48 +0530

    remove and update configs

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBY6hIFhRjQkYWEzU4Strb7xayIo-mPOFQ",
-  authDomain: "continentalhost-71577.firebaseapp.com",
-  projectId: "continentalhost-71577",
-  storageBucket: "continentalhost-71577.appspot.com",
-  messagingSenderId: "531773768761",
-  appId: "1:531773768761:web:a6454b25433f2d272a39e8",
-  measurementId: "G-R842BRV67S"
+  apiKey: "AIzaSyBw5aD3zPu7N6ChE4Cez4cSCrT4886tUug",
+  authDomain: "livemeteranalytics.firebaseapp.com",
+  projectId: "livemeteranalytics",
+  storageBucket: "livemeteranalytics.appspot.com",
+  messagingSenderId: "419699626349",
+  appId: "1:419699626349:web:376cefc3caba8495734a64",
+  measurementId: "G-D0TCDBNK1L",
 };
 
 // Initialize Firebase
